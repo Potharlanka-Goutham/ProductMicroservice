f43cc50ce04a4d460bac16eeb76be8eecb57d259
Potharlanka-Goutham <110443146+Potharlanka-Goutham@users.noreply.github.com>
2024-12-09 00:16:56 +0530

    Update ClientController.java

--- src/main/java/com/klef/jfsd/springboot/controller/ClientController.java
+++ src/main/java/com/klef/jfsd/springboot/controller/ClientController.java
@@ -2,12 +2,12 @@ package com.klef.jfsd.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
 
-@Controller
+@RestController
 public class ClientController {
 	
 	    @GetMapping("/")
